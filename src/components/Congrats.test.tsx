bb57d25372ec7d2de1e151a89234e95bca6a1b8a
Francisco <javiermartincuenca@gmail.com>
2019-10-12 19:11:37 +0200

    Congrats component  added

---
+++ src/components/Congrats.test.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Enzyme, { shallow, ShallowWrapper } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
+import Congrats from './Congrats';
+import { findByTestAttr } from '../../test/testUitls';
+
+Enzyme.configure({adapter: new EnzymeAdapter()});
+
+/**
+ * Factory function to create a ShallowWrapper for the Congrats component.
+ * @function setup
+ * @param props - Component props specific to this setup
+ * @returns {ShallowWrapper}
+ */
+const setup = (props ={}) => {
+    return shallow(<Congrats {...props}/>)
+};
+
+test('renders without error', () => {
+    const wrapper = setup();
+    const component = findByTestAttr(wrapper, 'component-congrats');
+    expect(component.length).toBe(1);
+});
+
+test('renders no text when `success` props is false', () => {
+    const wrapper = setup({success: false});
+    const component = findByTestAttr(wrapper, 'component-congrats');
+    expect(component.text()).toBe('');
+});
+
+test('renders non-empty congrats message when sucess prop is true', () => {
+    const wrapper = setup({success: true});
+    const message = findByTestAttr(wrapper, 'component-congrats');
+    expect(message.text().length).not.toBe(0);
+});

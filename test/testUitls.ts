1478d343d6a850889a84efc805a11c58f3541fbd
Francisco <javiermartincuenca@gmail.com>
2019-10-14 16:10:28 +0200

    Install redux thunk

--- test/testUitls.ts
+++ test/testUitls.ts
@@ -1,6 +1,7 @@
 import { ShallowWrapper } from "enzyme";
-import { createStore } from 'redux';
+import { createStore, Store, applyMiddleware } from 'redux';
 import rootReducer from '../src/reducers';
+import { middlewares } from '../src/configureStore';
 
 /**
  * Return node(s) with the given data-test attribute.
@@ -8,18 +9,17 @@ import rootReducer from '../src/reducers';
  * @param val - Value of data-tet attribute for search.
  * @returns {ShallowWrapper}
  */
-export const findByTestAttr = (wrapper: ShallowWrapper, val: string): ShallowWrapper => {
-    return wrapper.find(`[data-test="${val}"]`);
-}
+export const findByTestAttr = (wrapper: ShallowWrapper, val: string): ShallowWrapper =>
+    wrapper.find(`[data-test="${val}"]`);
 
 /**
  * Create a testing store with imported reducers, middleware, and initial state.
- * globals: rootReducers.
+ * globals: rootReducers, middlewares.
  * @function storeFactory
  * @param initialState - Initial state for store.
  * @returns {Store} - Redux store.
  */
-export const storeFactory = (initialState: any) => {
-    console.log('InitialState2', initialState);
-    return createStore(rootReducer, initialState);
+export const storeFactory = (initialState: any): Store =>{
+    const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore);
+    return createStoreWithMiddleware(rootReducer, initialState);
 }

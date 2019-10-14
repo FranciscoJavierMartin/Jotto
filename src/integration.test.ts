7f626b9167a611cd27135f4c7908d95321f0b76e
Francisco <javiermartincuenca@gmail.com>
2019-10-14 17:15:55 +0200

    Setup guessWord tests

---
+++ src/integration.test.ts
@@ -0,0 +1,24 @@
+import { storeFactory } from '../test/testUitls';
+import { guessWord } from './actions';
+
+describe('guessedWord action dispatcher', () => {
+  describe('no guessed words', () => {
+    test('updates state correctly for unsuccessful guess', () => {
+
+    });
+
+    test('updates state correctly for successful guess', () => {
+
+    });
+  });
+
+  describe('some guessed words', () => {
+    test('updates state correctly for unsuccessful guess', () => {
+
+    });
+
+    test('updates state correctly for successful guess', () => {
+
+    });
+  });
+});
--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import success from './successReducer';
+import guessWords from './guessedWordsReducers';
 
 export default combineReducers({
   success,
+  guessWords,
 })

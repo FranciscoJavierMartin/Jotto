fed12c5cdb9ecdab67f607ded6969b45dfa77bb5
Francisco <javiermartincuenca@gmail.com>
2019-10-14 12:35:46 +0200

    Success reducer addex

--- src/reducers/successReducer.ts
+++ src/reducers/successReducer.ts
@@ -1,9 +1,21 @@
+import { actionTypes } from '../actions';
+
 /**
  * @function successReducer
  * @param {array} state - Array of guessed words.
  * @param {object} action - action to be reduced.
  * @returns {boolean} - new success state.
  */
-export default (state, action) => {
-  return null;
+export default (state = false, action: any) => {
+  let newState;
+  
+  switch(action.type){
+    case actionTypes.CORRECT_GUESS:
+      newState = true;
+      break;
+    default:
+      newState = false;
+  }
+
+  return newState;
 } 
